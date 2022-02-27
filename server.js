df3624c4213c09ac83eb36c88f04294c9a5e05e9
Jo Aqra <yousif.aqra@gmail.com>
2022-02-27 22:14:07 +0200

    add logger middlerware

--- server.js
+++ server.js
@@ -13,39 +13,46 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL_USER,
-    serviceClient: process.env.EMAIL_CLIENT_ID,
-    privateKey: process.env.EMAIL_PRIVATE_KEY,
-  },
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL_USER,
+        serviceClient: process.env.EMAIL_CLIENT_ID,
+        privateKey: process.env.EMAIL_PRIVATE_KEY,
+    },
 });
+
+app.use((req, res, next) => {
+    console.log('request from:', req.ip);
+    next();
+})
 
 app.post("/contact", async (req, res, next) => {
-  // { email, name, subject, message }
-  const data = { ...req.body, date: new Date(Date.now()) };
+    // { email, name, subject, message }
+    const data = { ...req.body, date: new Date(Date.now()) };
 
-  let html = await readFile("./email.html", "utf8");
-  let template = handlebars.compile(html);
-  let htmlToSend = template(data);
+    let html = await readFile("./email.html", "utf8");
+    let template = handlebars.compile(html);
+    let htmlToSend = template(data);
 
-  await transporter.sendMail({
-    to: "info@joinnextstep.com",
-    from: data.email,
-    subject: "Connection request from NextStep landing page!",
-    html: htmlToSend,
-  });
-
-  res
-    .status(201)
-    .json({
-      status: "success",
-      message:
-        "Thank you for your message, we will make sure to back to you soon!",
+    await transporter.sendMail({
+        to: "info@joinnextstep.com",
+        from: data.email,
+        subject: "Connection request from NextStep landing page!",
+        html: htmlToSend,
     });
+
+    res
+        .status(201)
+        .json({
+            status: "success",
+            message:
+                "Thank you for your message, we will make sure to back to you soon!",
+        });
 });
 
-app.listen(3200);
+app.listen(3200, () => {
+    console.log('server is running!');
+});
